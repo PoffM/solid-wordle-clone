25dc908f25f8d41345203a62256a13283582c1a0
Mathew Poff <matp@live.ca>
2022-07-21 14:19:55 -0400

    Copied and converted the rest of the WordleGame tests from the React project.

--- src/components/WordleGame.test.tsx
+++ src/components/WordleGame.test.tsx
@@ -122,4 +122,102 @@ describe("WordleGame component", () => {
       expect(letterBoxes.filter((node) => node.innerText)).toEqual([]);
     }
   });
+
+  it("Plays through a game where you lose", async () => {
+    const { baseElement } = render(() => <WordleGame solution="HELLO" />);
+
+    // Guess with all wrong letters 6 times:
+    for (let i = 1; i <= 6; i++) {
+      for (const key of ["a", "m", "i", "s", "s", "Enter"]) {
+        fireEvent.keyDown(baseElement, { key });
+      }
+    }
+
+    // Post-lose UI:
+    {
+      // The solution text is shown:
+      expect(screen.getByText("SOLUTION")).toBeTruthy();
+      expect(screen.getByText("HELLO")).toBeTruthy();
+
+      // Click the restart button:
+      screen.getByRole("button", { name: "Next Word" }).click();
+      const letterBoxes = screen.getAllByTestId("letter-box");
+
+      // All the boxes are blank again:
+      expect(letterBoxes.filter((node) => node.innerText)).toEqual([]);
+    }
+  });
+
+  it("Enters the text using the clickable keyboard UI", async () => {
+    render(() => <WordleGame solution="HELLO" />);
+
+    // Guess "OLLIE" because it has a mix of different results:
+    screen.getByRole("button", { name: "O" }).click();
+    screen.getByRole("button", { name: "L" }).click();
+    screen.getByRole("button", { name: "L" }).click();
+    screen.getByRole("button", { name: "I" }).click();
+    screen.getByRole("button", { name: "E" }).click();
+    screen.getByRole("button", { name: /enter/i }).click();
+
+    const firstRowBoxes = screen.getAllByTestId("letter-box").slice(0, 5);
+
+    // The guess is rendered in the second row:
+    expect(firstRowBoxes.map((node) => node.textContent?.[0])).toEqual([
+      "O",
+      "L",
+      "L",
+      "I",
+      "E",
+    ]);
+
+    // The correct letter box colors are shown:
+    await waitFor(() => {
+      expect(
+        firstRowBoxes.map((node) => node.getAttribute("data-background-color"))
+      ).toEqual([
+        "bg-yellow-500", // O: misplaced
+        "bg-yellow-500", // First L: misplaced
+        "bg-green-500", // Second L: correct spot
+        "bg-gray-500 dark:bg-gray-600", // I: Not in the solution
+        "bg-yellow-500", // E: misplaced
+      ]);
+    });
+  });
+
+  it("Shows a toast message when you enter an unknown word", async () => {
+    const { baseElement } = render(() => <WordleGame solution="HELLO" />);
+
+    for (const key of ["a", "s", "d", "f", "g", "Enter"]) {
+      fireEvent.keyDown(baseElement, { key });
+    }
+
+    expect(screen.getByText("Word not in word list.")).toBeTruthy();
+  });
+
+  it("Shows a toast message when you enter a word that's too short", async () => {
+    const { baseElement } = render(() => <WordleGame solution="HELLO" />);
+
+    for (const key of ["r", "e", "d", "Enter"]) {
+      fireEvent.keyDown(baseElement, { key });
+    }
+
+    expect(screen.getByText("Not enough letters.")).toBeTruthy();
+  });
+
+  it("Lets you remove a letter by pressing backspace", async () => {
+    const { baseElement } = render(() => <WordleGame solution="HELLO" />);
+
+    for (const key of ["r", "e", "d", "Backspace"]) {
+      fireEvent.keyDown(baseElement, { key });
+    }
+
+    // The guess is rendered in the first row:
+    const firstRowBoxes = screen.getAllByTestId("letter-box").slice(0, 3);
+    expect(firstRowBoxes.map((node) => node.textContent?.[0])).toEqual([
+      "R",
+      "E",
+      undefined,
+    ]);
+  });
+
 });
